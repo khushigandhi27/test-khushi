a402a7dd7356d96787b3703b9d6daf22b0a292f2
khushigandhi27 <khushi.dreamkasper@gmail.com>
2024-01-29 12:05:34 +0530

    changes made

--- src/controllers/books/delete-books.js
+++ src/controllers/books/delete-books.js
@@ -1,37 +1,20 @@
+const httpStatus = require('http-status');
 const { deleteBooks } = require('../../database-functions/del-books');
+const ApiError = require('../../utils/ApiError');
 const generateResponse = require('../../utils/generate-response');
 
 const deleteBookController = async (req, res, next) => {
 
     try {
         const { id = '' } = req.body;
-
         if (!id) {
-            // Handle case where id is not provided
-            return res.status(400).send(generateResponse("Invalid or missing book ID"));
+            throw new ApiError(httpStatus[400],"not found data");
         }
-
         await deleteBooks(id);
         return res.send(generateResponse("Book Deleted"));
     } catch (error) {
-        console.error('Error deleting book:', error);
-        return res.status(500).send({ error: 'Error deleting book', details: error });
-        // return next(error);
+        return next(error);
     }
-
-    // try{
-    //     const{
-    //         body: {id = ''}} = req;
-
-    //         const data = {id};
-
-    //         await deleteBooks(data);
-    //         return res.send(generateResponse("Book Deleted"));
-    // } catch (error) {
-    //     console.error('Error deleting book:', error);
-    //     return { error: 'Error deleting book', details: error };
-    //     //return next(error);
-    // }
 };
 
 module.exports = deleteBookController;
