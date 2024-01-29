82594a45d37ca25a35ae5bfc9ae52a819c1bb07c
khushigandhi27 <khushi.dreamkasper@gmail.com>
2024-01-29 12:06:37 +0530

    changes made

--- src/database-functions/del-books.js
+++ src/database-functions/del-books.js
@@ -5,12 +5,10 @@ const db = admin.firestore();
 const deleteBooks = async bookData => {
     try{
         console.log('Deleting book with ID:', bookData);
-        //console.log(bookData);
         await db.collection(COLLECTIONS.BOOKS).doc(bookData).delete();
         console.log('Book deleted successfully.');
         return true;
     } catch (error) {
-        console.error('Error deleting book:', error);
         return error;
     }
 };
